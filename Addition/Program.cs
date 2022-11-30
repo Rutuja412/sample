d8074f375eb94f8cd417f058ee82921c16fed928
Rutuja412 <rutujakadam4545@gmail.com>
2022-11-30 16:57:17 +0530

    Adding addition program

---
+++ Addition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2=3;
+            int sum=num1+ num2;
+            Console.WriteLine("sum:"+sum);
+        }
+    }
+}
